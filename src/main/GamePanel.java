afe0b618ceb8ea7617cc4f82950f3862454d4398
mefernandez <mariano.fernandez@gmail.com>
2024-08-23 09:14:26 +0200

    Some code to start

---
+++ src/main/GamePanel.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+	
+	final int originalTileSize = 16; // 16x16 tile
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale; // 48x48
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	
+	Thread gameThread;
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+	}
+	
+	public void startGameThread() {
+		this.gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+		// GAME LOOP
+		
+	}
+
+}
