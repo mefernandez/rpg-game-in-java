9698230d965c8134bc23aa30d217f54043283364
mefernandez <mariano.fernandez@gmail.com>
2024-08-24 18:41:27 +0200

    Mi foto

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,12 +7,14 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class GamePanel extends JPanel implements Runnable {
 	
 	final int originalTileSize = 16; // 16x16 tile
 	final int scale = 3;
 	
-	final int tileSize = originalTileSize * scale; // 48x48
+	public final int tileSize = originalTileSize * scale; // 48x48
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -20,6 +22,7 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+	Player player = new Player(this, keyH);
 	
 	// Set player's default position
 	int playerX = 100;
@@ -69,27 +72,16 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 	
 	public void update() {
-		if (keyH.upPressed == true) {
-			playerY -= playerSpeed;
-			
-		} else if (keyH.downPressed == true) {
-			playerY += playerSpeed;
-			
-		} else if (keyH.leftPressed == true) {
-			playerX -= playerSpeed;
-			
-		} else if (keyH.rightPressed == true) {
-			playerX += playerSpeed;
-			
-		}
+		player.update();
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.white);
-		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		
+		player.draw(g2);
+		
 		g2.dispose();
 	}
 
