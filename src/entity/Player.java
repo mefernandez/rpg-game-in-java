ae26b20bc502d750a814b27ff33e51c454bb8bd1
LEO <Leo@iMac-de-Angie.home>
2024-08-25 17:28:02 +0200

    pixelart right1 transparente

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -38,8 +38,8 @@ public class Player extends Entity {
 			this.down2 = ImageIO.read(getClass().getResourceAsStream("/player/yo.png"));
 			this.left1 = ImageIO.read(getClass().getResourceAsStream("/player/yo.png"));
 			this.left2 = ImageIO.read(getClass().getResourceAsStream("/player/yo.png"));
-			this.right1 = ImageIO.read(getClass().getResourceAsStream("/player/pixel.png"));
-			this.right2 = ImageIO.read(getClass().getResourceAsStream("/player/yo.png"));
+			this.right1 = ImageIO.read(getClass().getResourceAsStream("/player/pixelright1.png"));
+			this.right2 = ImageIO.read(getClass().getResourceAsStream("/player/pixel1or2right.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
